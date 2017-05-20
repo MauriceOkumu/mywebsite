49d8906b90bd1a1c9d693b00e1011ec22ca8a076
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-05-20 15:26:11 -0700

    (feat) Serve index.html file server side

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var path = require('path');
+var morgan = require('morgan');
+var bodyparser = require('body-parser');
 
 var port = 3009;
+
 var server = express();
+server.use(morgan('dev'));
+server.use(bodyparser.json());
+server.use(bodyparser.urlencoded({extended: true}));
+server.use(express.static(__dirname + '/../client'));
 
 server.listen(port, function() {
   console.log(' The server is listening now on port :',port);	
-})
+});
