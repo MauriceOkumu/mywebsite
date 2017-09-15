5ce6c0e4879b4efc72c57b83130e8e20cf166568
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-09-15 15:08:47 -0700

    Add classless component

--- client/app/about.jsx
+++ client/app/about.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './about.css'
 
-class About extends React.Component {
-  constructor(props) {
-  	super(props)
-  }
-  render () {
-  	return (
-  	  <div className={styles.container}>This is where I will talk about myself</div>
-  	)
-  }
-}
+const About = ()=> {
+	return (<div className={styles.container}>
+        <p>I am a Kenyan by birth. Growing up I liked playing basketball 
+        and watching movies, especially animated movies and cartoons. I was and still am 
+        an avid enthusiast of video games, especially the classic arcade games.</p>
+        <p>
+        	I worked at the Atlantis hotel in Dubai for 5 years before moving to the USA
+        	with my lovely wife, whom I met while working in Dubai.
+        </p>
+  	  </div>)
+  	}
+
 
 export default About;
