e4f0b23b1dc9117edc4b36e1f04af22df5331a80
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-09-15 17:35:43 -0700

    Add About Me

--- client/app/navbar.jsx
+++ client/app/navbar.jsx
@@ -6,7 +6,7 @@ const NavBarIn = () => (
     <div className="container">
       <div id="navbar" className="collapse navbar-collapse">
         <ul className="nav navbar-nav navbar-right">
-          <li><a href="/feed">Home</a></li>
+          <li><a href="/feed">About Me</a></li>
           <li><a href="/profile">Profile</a></li>
           <li><a href="/logout">Contact Me</a></li>
         </ul>
