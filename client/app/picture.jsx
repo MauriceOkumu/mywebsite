c914ae3288ae64d5c9ecfac46ec29a9c398fc8da
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-09-15 17:36:24 -0700

    Clean up

--- client/app/picture.jsx
+++ client/app/picture.jsx
@@ -6,7 +6,7 @@ class Picture extends React.Component{
   }	
   render() {
   	return (
-  	  <div><img src="file:///Users/admin/Desktop/IMG_3222.jpg" alt="Pictures will go here"></img></div>	
+  	  <div></div>	
   	)
   }
 }
