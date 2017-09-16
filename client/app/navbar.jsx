836da943476b2fdbb4dfbc9320df3d4a2293ebec
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-09-15 17:07:43 -0700

    Add css

--- client/app/navbar.jsx
+++ client/app/navbar.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import styles from './navbar.css';
 
 const NavBarIn = () => (
-  <nav className="navbar navbar-default">
+  <nav className={styles.nav}>
     <div className="container">
       <div id="navbar" className="collapse navbar-collapse">
         <ul className="nav navbar-nav navbar-right">
           <li><a href="/feed">Home</a></li>
           <li><a href="/profile">Profile</a></li>
-          <li><a href="/settings">About</a></li>
           <li><a href="/logout">Contact Me</a></li>
         </ul>
       </div>
