5e48bd60a72a84ea3ac4bb27b4097edb16bcc3a8
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-09-20 14:31:00 -0700

    Add Lightbox

--- client/app/picture.jsx
+++ client/app/picture.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
-import Pic from './pics/IMG_3222.jpg'
+// import Lightbox from 'react-images'
+import Pic from './pics/pic1.jpg'
 
 class Picture extends React.Component{
   constructor(props) {
   	super(props)
+    this.state = {
+      lightboxIsOpen: false,
+      imageStatus: 'Loading'
+
+    }
+    this.closeLightbox = this.closeLightbox.bind(this);
   }	
+  closeLightbox () {
+    this.setState({
+      lightboxIsOpen:false
+    })
+  }
+  handleImageLoaded() {
+    this.setState({ imageStatus: 'loaded' });
+  }
+ 
+  handleImageErrored() {
+    this.setState({ imageStatus: 'failed to load' });
+  }
   render() {
+    console.log(Pic)
   	return (
-  	  <div>
-  	  	<img src={Pic}/>
-  	  </div>	
-  	)
+      <div>
+      <img 
+       src={ Pic } alt={"Picture1"}
+       onLoad={this.handleImageLoaded.bind(this)}
+       onError={this.handleImageErrored.bind(this)}
+      />
+       { this.state.imageStatus }
+      </div>
+    )
   }
 }
 
 export default Picture;
+  	  // <Lightbox
+     //    images={[{ src: Pic }]}
+     //    isOpen={!this.state.lightboxIsOpen}
+     //    onClickPrev={this.gotoPrevious}
+     //    onClickNext={this.gotoNext}
+     //    onClose={this.closeLightbox}
+     //  />	
